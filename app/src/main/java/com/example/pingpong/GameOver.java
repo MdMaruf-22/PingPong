75832c91f416083efc6848d4a6a8074aa233f941
Maruf <marufislam74208@gmail.com>
2024-01-29 03:38:49 +0600

    Created the xml for game oevr

---
+++ app/src/main/java/com/example/pingpong/GameOver.java
@@ -0,0 +1,14 @@
+package com.example.pingpong;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameOver extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
